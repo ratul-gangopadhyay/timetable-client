f4046c4386b13c62503fda67a596b8206b434ea7
Ratul Ganguly <rgratulganguly1234@outlook.com>
2022-08-15 08:33:03 +0530

    Corrected button name and code cleanup

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from '@mui/material';
 import './teacherStyles.css';
 import TeachersTableContainer from './TeachersTable.container';
 import TeacherFormModalContainer from './TeacherFormModal.container';
@@ -7,22 +6,10 @@ import TeacherFormModalContainer from './TeacherFormModal.container';
 const Teachers = () => {
   return (
     <div className='teacher-container'>
-      {/* <h1>Teachers</h1> */}
-      <div className='teacher-header-container'>
-        {/* <span>Teacher</span> */}
-      </div>
+      <div className='teacher-header-container'></div>
       <div className='teachers-table-container'>
         <TeachersTableContainer />
       </div>
-      {/* <div className='add-icon-btn-container'>
-      <Button
-          sx={{ backgroundColor: '#313437', fontFamily: 'Calibri' }}
-          variant='contained'
-          size='small'
-        >
-          Add New Teacher
-        </Button>
-      </div> */}
       <TeacherFormModalContainer />
     </div>
   );
